cdbd3ff2f3f26d6fda79ab42cc441520c3b109bc
danielv775 <danielv775@gmail.com>
2021-08-08 16:26:06 -0400

    starting indicators

--- src/etl/technical_indicators.py
+++ src/etl/technical_indicators.py
@@ -2,6 +2,26 @@
 
 # technical indicator functions
 
+def calc_sma(data, window):
+    '''
+    simple moving average
+    '''
+    sma = data['close'].rolling(window).mean()
+    return sma
+        
+def calc_bb_width(data, window, scale=2):
+    '''
+    bollinger bands width
+    '''
+    bb_width = data['close'].rolling(window).std() * scale
+    return bb_width
+
+def calc_ror(data, periods):
+    '''
+    rate of return (pct change over period)
+    '''
+    ror = data['close'].pct_change(periods=periods, fill_method='ffill')
+    return ror 
 
 if __name__ == '__main__':
     pass
